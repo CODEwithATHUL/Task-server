6ddd6f855ef67891aacdbbd0fdb83b01355b7f73
ATHUL P KANNAN <athulputhettu1234@gmail.com>
2025-01-16 13:19:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const jsonServer = require("json-server")
-const TMServer = jsonServer.create()
+const jsonServer = require('json-server');
+const TMServer = jsonServer.create();
+const middleWare = jsonServer.defaults();
+const route = jsonServer.router('db.json');
 
-const middleWare = jsonServer.defaults()
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
-const route = jsonServer.router('db.json')
+TMServer.use(middleWare);
+TMServer.use(route);
 
-TMServer.use(middleWare)
-TMServer.use(route)
-
-TMServer.listen(PORT,()=>{
-    console.log(`TMServer started running at port : ${PORT} and its waiting for client request `)
-})
+TMServer.listen(PORT, () => {
+    console.log(`TMServer started running at port : ${PORT} and its waiting for client request`);
+});
